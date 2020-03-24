d30df5886ed81003fbd0db60dff0c957dc6ee07d
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-03-24 01:06:36 +0100

    Adding played moves to the history component

--- frontend/src/components/GamePage.vue
+++ frontend/src/components/GamePage.vue
@@ -6,8 +6,10 @@
       :white_player_human="whitePlayerHuman"
       :black_player_human="blackPlayerHuman"
       :reversed="boardReversed"
+      @move_done="addMoveToHistory"
     ></loloof64-chessboard>
-    <move-history ref="history" :history="history" class="mx-2"></move-history>
+    <move-history ref="history" :history="orderedHistory"
+          @position_requested="setPosition($event)" class="mx-2"></move-history>
   </v-layout>
 </template>
 
